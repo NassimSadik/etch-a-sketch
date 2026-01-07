eb2b9eb06ede21913ea28380a0d4d30b07a044fe
Nassim <167772440+NassimSadik@users.noreply.github.com>
2026-01-07 14:11:43 +0100

    Create 16x16 grid of square divs

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container');
+
+function createGrid() {
+  const squaresPerSide = 16;
+  const gridSize = squaresPerSide * squaresPerSide;
+  const CONTAINERSIZE = 960;
+
+  for (let i = 0; i < gridSize; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `${CONTAINERSIZE / squaresPerSide}px`;
+    square.style.height = `${CONTAINERSIZE / squaresPerSide}px`;
+
+    container.appendChild(square);
+  }
+}
+
+createGrid();
