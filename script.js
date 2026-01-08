9c755bc1db8a4da75fdde8676e9fa5219749d6a7
Nassim <167772440+NassimSadik@users.noreply.github.com>
2026-01-08 17:52:55 +0100

    Add customization of the number of squares per side through a prompt

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const container = document.querySelector('.container');
 
-function createGrid() {
-  const squaresPerSide = 16;
+function createGrid(squaresPerSide) {
   const gridSize = squaresPerSide * squaresPerSide;
   const CONTAINERSIZE = 960;
 
@@ -19,4 +18,31 @@ function createGrid() {
   }
 }
 
-createGrid();
+createGrid(16);
+
+const gridButton = document.querySelector('.grid-button');
+
+function handleGridButton() {
+  const input = prompt(
+    'Enter the desired number of squares per side between 8 and 100',
+    '16'
+  );
+
+  const squaresPerSide = parseInt(input);
+
+  if (squaresPerSide < 8 || squaresPerSide > 100 || isNaN(squaresPerSide)) {
+    alert('Please enter a valid number between 8 and 100');
+    return;
+  }
+
+  // Remove existing grid
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  createGrid(squaresPerSide);
+}
+
+gridButton.addEventListener('click', () => {
+  handleGridButton();
+});
